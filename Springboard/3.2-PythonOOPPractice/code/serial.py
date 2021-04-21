908351bf3f7a04df154696086c763cc9fee278bb
jsperson <jsperson@gmail.com>
2021-04-21 10:21:31 -0500

    Completed code per spec. Added test files

--- Springboard/3.2-PythonOOPPractice/code/serial.py
+++ Springboard/3.2-PythonOOPPractice/code/serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -18,5 +19,54 @@ class SerialGenerator:
 
     >>> serial.generate()
     100
+
+    >>> serial
+    <SerialGenerator start=100 next=101>
     """
 
+    initial_val = 0
+
+    def __init__(self, start=0):
+        """Constructor
+
+        Args:
+            start (int): initial start value for the serial sequence
+        """
+        self.start = start
+        # Stash away the start value for resetting
+        self.initial_val = start
+
+    def __repr__(self):
+        return_val = '<SerialGenerator start=' + \
+            str(self.initial_val) + ' next=' + str(self.start) + '>'
+        return return_val
+
+    def generate(self):
+        """Return existing value then increments by 1.
+
+        Returns:
+            int : exising value"""
+        self.start += 1
+        return self.start - 1
+
+    def reset(self):
+        """Resets existing value to original start value"""
+        self.start = self.initial_val
+
+
+if __name__ == '__main__':
+    # Test harness copied from https://docs.python.org/3/library/doctest.html
+    import doctest
+    import sys
+    flags = doctest.REPORT_NDIFF | doctest.FAIL_FAST
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        if name in globals():
+            obj = globals()[name]
+        else:
+            obj = __test__[name]
+        doctest.run_docstring_examples(obj, globals(), name=name,
+                                       optionflags=flags)
+    else:
+        fail, total = doctest.testmod(optionflags=flags)
+        print("{} failures out of {} tests".format(fail, total))
--- Springboard/3.2-PythonOOPPractice/code/wordfinder.py
+++ Springboard/3.2-PythonOOPPractice/code/wordfinder.py
@@ -1,5 +1,68 @@
 """Word Finder: finds random words from a dictionary."""
+import random
 
 
 class WordFinder:
-    ...
+    """[summary]
+    >>> wf = WordFinder("./test.txt"); len(wf.lines) > 0
+    4 words read
+    True
+
+    >>> wf.random() in wf.lines
+    True
+
+    """
+
+    def __init__(self, filename):
+        def print_count(self, count):
+            print(str(count) + ' words read')
+
+        self.lines = self.parse_file(filename)
+
+        print_count(self, len(self.lines))
+
+    def random(self):
+        """Return random value from the list
+        """
+        return random.choice(self.lines)
+
+    def parse_file(self, filename):
+        f = open(filename, 'r')
+        lines = [line.strip() for line in f]
+        f.close()
+        return lines
+
+
+class SpecialWordFinder(WordFinder):
+    """
+    >>> swf = SpecialWordFinder("./special.txt")
+    4 words read
+
+    >>> swf.random() in swf.lines
+    True
+    """
+
+    def parse_file(self, filename):
+        f = open(filename, 'r')
+        lines = [line.strip() for line in f if len(
+            line.strip()) > 0 and line[0] != '#']
+        f.close()
+        return lines
+
+
+if __name__ == '__main__':
+    # Test harness copied from https://docs.python.org/3/library/doctest.html
+    import doctest
+    import sys
+    flags = doctest.REPORT_NDIFF | doctest.FAIL_FAST
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        if name in globals():
+            obj = globals()[name]
+        else:
+            obj = __test__[name]
+        doctest.run_docstring_examples(obj, globals(), name=name,
+                                       optionflags=flags)
+    else:
+        fail, total = doctest.testmod(optionflags=flags)
+        print("{} failures out of {} tests".format(fail, total))
